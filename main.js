82aa069a8fad05955c157dc3e893bbc1c55fe971
Andrea Di Cataldi <andrea.dicataldi@gmail.com>
2020-11-28 17:55:58 +0100

    Key Arrows
    key arrows now working

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(function () {
-  $(".next").click(function () {
+  function nextImg() {
     if ($(".last").hasClass("active") == true) {
       $(".first").addClass("active");
       $(".last").removeClass("active");
@@ -7,14 +7,27 @@ $(function () {
       $(".active").next().addClass("active");
       $(".active").prev().removeClass("active");
     }
-  });
-  $(".prev").click(function () {
+  }
+  function prevImg() {
     if ($(".first").hasClass("active") == true) {
       $(".last").addClass("active");
       $(".first").removeClass("active");
     } else {
       $(".active").prev().addClass("active");
       $(".active").next().removeClass("active");
+    }
+  }
+  $(".next").click(function () {
+    nextImg();
+  });
+  $(".prev").click(function () {
+    prevImg();
+  });
+  $(document).keydown(function (e) {
+    if (e.keyCode == 39) {
+      nextImg();
+    } else if (e.keyCode == 37) {
+      prevImg();
     }
   });
 });
